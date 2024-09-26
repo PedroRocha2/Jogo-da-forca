6601f39e60326d6bf1cc94d20584591a99b23de7
PedroRocha2 <pedro.rocha.neni@gmail.com>
2024-09-26 12:36:31 -0300

    adicionando palavras aleatórias

--- jogo.h
+++ jogo.h
@@ -1,24 +1,54 @@
 #include <iostream>
 #include <cstring>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 class Jogodaforca {
 public:
     bool Adivinharletra(char palavra[], char jogo_atual[], char tentativa);
+    string GerarPalavra();
 };
 
 bool Jogodaforca::Adivinharletra(char palavra[], char jogo_atual[], char tentativa) {
-    bool encontrou = false;
+    bool encontrou = true;
     for (int i = 0; i < strlen(palavra); i++) {
         if (palavra[i] == tentativa) {
             jogo_atual[i] = palavra[i];
-            encontrou = true;
         }
     }
 
     if (strcmp(palavra, jogo_atual) == 0) {
-        return false; 
+        cout << "Parabéns! Você adivinhou a palavra" << endl;
+        encontrou = false;
     }
     return encontrou; 
+}
+
+string Jogodaforca::GerarPalavra() {
+    fstream fs("palavras.txt", ios::in | ios::out | ios::trunc);
+    vector<string> palavras;
+    string palavra;
+
+    if (!fs) {
+        cerr << "Erro ao abrir o arquivo: " << endl;
+        return "";
+    }
+
+    while (getline(fs, palavra)) {
+        palavras.push_back(palavra);
+    }
+    fs.close();
+
+    if (palavras.empty()) {
+        cerr << "O arquivo está vazio ou não contém palavras válidas." << endl;
+        return "";
+    }
+
+    srand(time(0));
+    int indiceAleatorio = rand() % palavras.size();
+    return palavras[indiceAleatorio];
 }
--- jogodaforca.cpp
+++ jogodaforca.cpp
@@ -4,35 +4,50 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
+    Jogodaforca f;
     string palavraSorteada;
     char *p_palavrasorteada, *jogo_atual;
     char tentativa;
     bool resultado = true;
 
-    cout << "Escolha uma palavra: ";
-    cin >> palavraSorteada;
+    palavraSorteada = f.GerarPalavra();
+
+    if (palavraSorteada.empty())
+    {
+        cerr << "Erro ao gerar palavra." << endl;
+        return 1;
+    }
 
     p_palavrasorteada = new (nothrow) char[palavraSorteada.length() + 1];
-    if (p_palavrasorteada == nullptr) {
+    
+    if (p_palavrasorteada == nullptr)
+    {
         cerr << "Erro ao alocar memória para a palavra sorteada." << endl;
         return 1;
     }
+    
     strcpy(p_palavrasorteada, palavraSorteada.c_str());
-  
+
     jogo_atual = new (nothrow) char[palavraSorteada.length() + 1];
-    if (jogo_atual == nullptr) {
+    
+    if (jogo_atual == nullptr)
+    {
         cerr << "Erro ao alocar memória para o array de tentativas." << endl;
         delete[] p_palavrasorteada;
         return 1;
     }
-    for (size_t i = 0; i < palavraSorteada.length(); ++i) {
+    
+    for (size_t i = 0; i < palavraSorteada.length(); ++i)
+    {
         jogo_atual[i] = '_';
     }
+    
     jogo_atual[palavraSorteada.length()] = '\0';
 
-    Jogodaforca f;
-    while (resultado) {
+    while (resultado)
+    {
         cout << "Adivinhe uma letra: ";
         cin >> tentativa;
         resultado = f.Adivinharletra(p_palavrasorteada, jogo_atual, tentativa);
